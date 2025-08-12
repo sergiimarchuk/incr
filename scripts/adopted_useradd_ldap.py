ec74b78c1f49424ac7c07018568ded72251cb4fc
sergii.marchuk <sergii.marchuk@gmail.com>
2025-08-12 12:41:08 +0200

    updated scripts/adopted_useradd_ldap.py

--- scripts/adopted_useradd_ldap.py
+++ scripts/adopted_useradd_ldap.py
@@ -1,27 +1,21 @@
 import sys
-sys.path.insert(0, '/o/opt/dev-py/incredible/libs')  # adjust if needed
+sys.path.insert(0, '/opt/dev-py/incredible/libs')  # adjust if needed
 
 from ldap3 import Server, Connection, ALL
 import sys
+
+from ldap3 import Server, Connection, ALL
 
 LDAP_SERVER = 'ldap://localhost:8389'
 ADMIN_DN = 'cn=admin,dc=myorg,dc=local'
 ADMIN_PASSWORD = 'admin'
 
-# New user info
-<<<<<<< HEAD
+# New user info: brooks
 USER_UID = 'brooks'
-USER_CN = 'brooks'  # common name, can be full name or username
+USER_CN = 'brooks'         # common name, can be full name or username
 USER_GIVENNAME = 'brooks'  # example first name
 USER_SN = 'brooks'         # example surname
-USER_EMAIL = 'sergii@example.com'
-=======
-USER_UID = 'e2_b1'
-USER_CN = 'e2_b1'
-USER_GIVENNAME = 'Se'
-USER_SN = 'Iv'
-USER_EMAIL = 'se@example.com'
->>>>>>> 90d18e3 (Remove local LDAP config and data from repo)
+USER_EMAIL = 'brooks@example.com'
 USER_PASSWORD = '1qaz1'
 USER_DN = f'uid={USER_UID},ou=users,dc=myorg,dc=local'
 OU_DN = 'ou=users,dc=myorg,dc=local'
